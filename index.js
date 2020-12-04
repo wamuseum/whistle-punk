9e502a6cf10a5843400b455942eda43aa3b26e5b
andrewr <andrew.rowe@museum.wa.gov.au>
2020-12-04 15:57:45 +0800

    multiple changes
     - test for config windows
     - fix urls before using them

--- index.js
+++ index.js
@@ -5,49 +5,51 @@ const express = require('express')
 const config = require('config')
 
 require('dotenv').config()
-oakWindows = config.get('windows')
+if (! (oakWindows = config.has('windows') ? config.windows : false)) {
+  console.log('Error loading oakWindows config')
+  process.exit(1)
+}
+
 oakObjects = []
 
 function loadWindows () {
   let sslExceptions = config.get('sslexceptions')
   let displays = oak.getDisplays()
-  //let errors = oak.catchErrors()
-  //let log = oak.log
 
-  //console.log("Displays: ",JSON.stringify(displays))
   oakWindows.forEach(function(oakWindow, index){
     let opts = {
       ...oakWindow,
-      //size: displays[oakWindow.display].workArea.width + "x" + displays[oakWindow.display].workArea.height,
-      //x: displays[oakWindow.display].workArea.x,
-      //y: displays[oakWindow.display].workArea.y,
+      x: displays[oakWindow.display].workArea.x,
+      y: displays[oakWindow.display].workArea.y,
       sslExceptions:  sslExceptions
     }
-
-    opts.url = opts.url.startsWith("http") ? opts.url : 'file://' + join(__dirname,opts.url)
     if (opts.fullscreen) {
       delete opts.size
     }
-    //console.log("Options: ", opts)
     oakObjects[index] = oak.load(opts)
     oakObjects[index].on('unresponsive', function(event) {
-      this.reload()
+      console.log('page has become unresponsive')
+      this.location(this.opts.url)
     })
   })
+
 }
 
 // everything has to wait for the main ready event to fire
 oak.on('ready', () => {
-  let waitFor = oakWindows.map(value => value.url.startsWith("http") ? value.url : join(__dirname,value.url))
-  if (process.env.WAIT_ON) {
-    waitFor = process.env.WAIT_ON.split(";")
-  }
+  oakWindows.map(value => {
+    value.url = value.url.startsWith("http") ? value.url : join(__dirname, value.url)
+  })
+  let waitFor = oakWindows.map(value => value.url)
   console.log("waitFor: ", waitFor)
+  console.log(oakWindows)
   let opts = {
     resources: waitFor
   }
   waitOn(opts, function (err) {
-    if (err) { return handleError(err); }
+    if (err) {
+      return handleError(err);
+    }
     // once here, all resources are available
     loadWindows()
   });
