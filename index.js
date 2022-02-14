e9e1a6ca5935fa71c46a0b5c00f5d4fe3ad7e8c3
Andrew Rowe <rowead@gmail.com>
2022-02-14 20:04:00 +0800

    adding handling of extra URLs to wait for

--- index.js
+++ index.js
@@ -5,65 +5,10 @@ const os = require('os')
 const path = require('path')
 const waitOn = require('wait-on')
 
-let config = {};
-
-if (args.deBug) {
-  console.dir(args);
+if (!config.has('windows')) {
+  console.log('Error loading oakWindows config')
+  process.exit(1)
 }
-
-if (args?._?.[0]?.length && args?._?.[0]?.match(/\.js$/)) {
-  // hand over to javascript file that replaces whistle-punk
-  require(args?._?.[0]);
-}
-else {
-  // Continue with Whistle Punk...
-  if (args?._?.[0]?.length && args?._?.[0]?.match(/\.ya?ml$/)) {
-    try {
-      config = yaml.load(fs.readFileSync(path.resolve(args?._?.[0]),  'utf8'));
-    }
-    catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  }
-  else { // load single url using yargs params
-    if (args?._?.[0]?.length) {
-      config = {
-        windows: {
-          default: {}
-        }
-      }
-      config.windows.default.url =  args?._?.[0];
-      config.windows.default.frame = args?.frame;
-      if (!(args?.x || args?.y || args.width || args?.height)) {
-        config.windows.default.fullscreen = args?.fullscreen;
-        config.windows.default.kiosk = args?.kiosk;
-      }
-      config.windows.default.x = args?.x || 0;
-      config.windows.default.y = args?.y || 0;
-      config.windows.default.display = args?.display;
-      config.windows.default.width = args?.width;
-      config.windows.default.height = args?.height;
-      config.windows.default.alwaysOnTop = args.ontop;
-      if (args?.whiteListDomain) {
-        config.domainwhitelist = args?.whiteListDomain
-      }
-      console.log(config);
-    }
-    else {
-      console.log('Invalid');
-      process.exit(1);
-    }
-  }
-
-  const { app } = require('electron');
-  const {loadWindows} = require('./lib/window.js');
-  const { setUpServer } = require('./lib/server');
-
-  if (!config?.windows) {
-    console.log('Error loading oakWindows config')
-    process.exit(1)
-  }
 
   if (config?.server) {
     setUpServer(config);
@@ -82,26 +27,29 @@ else {
       }
     }
 
-    if (config?.waitforurls && config.waitforurls) {
-      /*
-       * Get a list of URI's needed for all the windows and use wait-for to wait
-       * until they are all available. eg. wait for a local apache instance to load.
-       */
-      let waitFor = [] //= config.windows.map(value => value.url)
-      for (var key in config.windows) {
-        waitFor.push(config.windows[key].url)
+  if (config.has('waitforurls') && config.waitforurls) {
+    /*
+     * Get a list of URI's needed for all the windows and use wait-for to wait
+     * until they are all available. eg. wait for a local apache instance to load.
+     */
+    let waitFor = [] //= config.windows.map(value => value.url)
+    for (var key in config.windows) {
+      waitFor.push(config.windows[key].url)
+    }
+    if (config.has('extrawaitforurls')) {
+      for (var key in config.extrawaitforurls) {
+        waitFor.push(config.extrawaitforurls[key].url)
       }
+    }
 
-      waitOn({resources: waitFor}, function (err) {
-        if (err) {
-          console.log(err)
-        }
-        // once here, all resources are available
-        loadWindows(config)
-      });
-    }
-    else {
-      loadWindows(config)
-    }
-  }).catch(console.error);
-}
+    waitOn({resources: waitFor}, function (err) {
+      if (err) {
+        console.log(err)
+      }
+      // once here, all resources are available
+      loadWindows()
+    });
+  } else {
+    loadWindows()
+  }
+})
