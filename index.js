0b2845d657ea376cbb3e18ffa0a5e35523a736e4
Andrew Rowe <rowead@gmail.com>
2023-04-03 20:02:31 +0800

    switch to yargs
     - move code to libs/
     - move server code to own file
     - load config from yaml specified in cli
     - load file/url otherwise

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
-process.env["NODE_CONFIG_DIR"] = __dirname + "/config/" + require('path').delimiter + __dirname + "/config-override/"
-const config = require('config')
-const loadWindows = require('./window.js')
-const oak = require('oak')
+const yaml = require('js-yaml');
+const fs   = require('fs');
+const args = require('./lib/args.js')
+const {loadWindows} = require('./lib/window.js');
+const { app } = require('electron');
 const os = require('os')
 const path = require('path')
 const waitOn = require('wait-on')
+const { setUpServer } = require('./lib/server');
 
-if (!config.has('windows')) {
+let config = {};
+
+if (args.deBug) {
+  console.dir(args);
+}
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+
+if (args?._?.[0]?.length && args?._?.[0].match(/\.ya?ml$/)) {
+  try {
+    config = yaml.load(fs.readFileSync(path.resolve(args?._?.[0]),  'utf8'));
+  }
+  catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+else {
+  if (args?._?.[0]?.length) {
+    config = {
+      windows: {
+        default: {
+          url: args?._?.[0],
+          display: 0,
+        }
+      }
+    }
+  }
+  else {
+    console.log('Invalid');
+    process.exit(1);
+  }
+}
+
+if (!config?.windows) {
   console.log('Error loading oakWindows config')
   process.exit(1)
 }
 
-oak.on('ready', () => {
+if (config?.server) {
+  setUpServer(config);
+}
+app.whenReady().then(() => {
   let filePrefix = os.platform() == 'win32' ? '' : 'file://'
 
   for (let key in config.windows) {
     if (config.windows[key] && config.windows[key].hasOwnProperty('url')) {
       config.windows[key].url = config.windows[key].url.startsWith("http") ? config.windows[key].url : filePrefix + path.join(__dirname, config.windows[key].url)
-    } else {
+    }
+    else {
       // window has been removed by setting to false
-      delete(config.windows[key])
+      delete (config.windows[key])
     }
   }
 
-  if (config.has('waitforurls') && config.waitforurls) {
+  if (config?.waitforurls && config.waitforurls) {
     /*
      * Get a list of URI's needed for all the windows and use wait-for to wait
      * until they are all available. eg. wait for a local apache instance to load.
@@ -45,9 +85,10 @@ oak.on('ready', () => {
         console.log(err)
       }
       // once here, all resources are available
-      loadWindows()
+      loadWindows(config)
     });
-  } else {
-    loadWindows()
   }
-})
+  else {
+    loadWindows(config)
+  }
+}).catch(console.error);
